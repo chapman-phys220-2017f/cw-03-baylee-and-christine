2366e6264e6798d75c8ec2aedac6d23080d9aef6
Baylee Mumma <mumma103@mail.chapman.edu>
2017-09-12 18:19:13 +0000

    Gaussian

--- gaussian.py
+++ gaussian.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 def g(x):
+    g = (1/math.sqrt(2*math.pi))*(math.exp(-x**2/2))
+    return g
+
 def interval(f, a, b, dx):
+    f = x
+    N = (b-a)/dx
+    intList = []
+    
+    
+    
 def integrate(i, dx):
