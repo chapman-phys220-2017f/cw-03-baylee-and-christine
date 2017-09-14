def45def3b446c7a84b87d80301fdcbb14b52720
Christine <outla100@mail.chapman.edu>
2017-09-14 18:12:03 +0000

    interval function

--- gaussian.py
+++ gaussian.py
@@ -3,11 +3,13 @@ def g(x):
     g = (1/math.sqrt(2*math.pi))*(math.exp(-x**2/2))
     return g
 
+def f(x):
+    return x
+
 def interval(f, a, b, dx):
-    f = x
-    N = (b-a)/dx
-    intList = []
-    
-    
-    
+    N = int((b-a)/dx)
+    intList =[f(a+i*dx) for i in range(0,N+1)]
+    return intList
+#interval(f,0,1,.1)
+
 def integrate(i, dx):
