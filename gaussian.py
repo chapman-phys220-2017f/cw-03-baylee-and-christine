8c2dd604b990974701dbeec53743cb7d99542d63
Baylee Mumma <mumma103@mail.chapman.edu>
2017-09-19 16:24:26 +0000

    trials

--- gaussian.py
+++ gaussian.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
+
 def g(x):
     g = (1/math.sqrt(2*math.pi))*(math.exp(-x**2/2))
     return g
 
 def interval(f, a, b, dx):
-    f = x
-    N = (b-a)/dx
     intList = []
+    N = int((b-a)/dx)
+    intList =[f(a+i*dx) for i in range(0,N+1)]
+    return intList
     
-    
-    
+        
 def integrate(i, dx):
+    n = len(i)
+    total = i[0]+i[n-1]
+    for r in range(1,n-1):
+        total += 2*i[r]
+	    total = (dx/2)*total
+    return total
+    
+    
+if __name__ == "__main__":
+    print(interval(f,0,1,.1))
--- test_gaussian.py
+++ test_gaussian.py
@@ -7,12 +7,9 @@ import math
 import gaussian
 
 def test_g():
-    """Tests g with following trials:
-        - g(0) ?= 1.0/sqrt(2pi)
-    """
     # Pre-computed correct value of g(0)
     actual = 1.0/math.sqrt(2*math.pi)
-    # Testing implementation
+    # Testing
     trial = gaussian.g(0)
     # Debug messages like this are only printed if the test fails
     print("Testing g(0): ",actual," ?= ",trial)
@@ -21,5 +18,38 @@ def test_g():
     nose.tools.assert_almost_equal(actual, trial, 4)
 
 def test_interval():
+    # Pre-computed correct value of interval(f,0,1,.2)
+    actual = [gaussian.g(0), gaussian.g(.2), gaussian.g(1)]
+    # Testing implementation
+    trial = gaussian.interval(gaussian.f,0,1,.2)
+    # Debug messages
+    print("Testing interval(f,0,1,0.5): ",actual," ?= ",trial)
+    #assert
+    for r in range(0,2):
+        nose.tools.assert_almost_equal(actual[r], trial[r], 4)
+
 def test_integrate():
+    # Pre-computed correct value of the integral
+    actual = (1/2)*(gaussian.g(-1)+2*gaussian.g(0)+2*gaussian.g(1)+gaussian.g(2))
+    # Testing
+    trial = gaussian.integrate(gaussian.interval(gaussian.f,-1,2,1),1)
+    # Debug
+    print("Testing integral from -1 to 1: ",actual," ?= ",trial)
+    #assert
+    nose.tools.assert_almost_equal(actual, trial)
+    
 def test_gauss_norm():
+    # Pre-computed correct value of integral(i,dx)
+    actual = 1.00000
+    # Testing
+    trial = gaussian.integrate(gaussian.interval(gaussian.f,-20,20,.1),.1)
+    #Debug
+    print("Testing integral of normalized Gaussian function from -20 to 20: ",actual," ?= ",trial)
+    #assert
+    nose.tools.assert_almost_equal(actual, trial, 4)
+    
+    
+    
+    
+    
+    
